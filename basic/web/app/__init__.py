6c18fc2986789697c4488f6cc33042b23d9bc0a5
wizath <kubajot93@gmail.com>
2019-01-26 00:39:47 +0100

    changed basic version for flask > 1.0

--- basic/web/app/__init__.py
+++ basic/web/app/__init__.py
@@ -6,7 +6,7 @@ from config import config
 
 def create_app(config_name=None):
     if config_name is None:
-        config_name = os.environ.get('APP_CONFIG', 'development')
+        config_name = os.environ.get('FLASK_ENV', 'development')
 
     app = Flask(__name__)
     app.config.from_object(config[config_name])
