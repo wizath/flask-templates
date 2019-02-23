380f4ddd0e804f20794d8a7d04c1d48b1668e8e9
wizath <kubajot93@gmail.com>
2019-02-23 13:28:42 +0100

    React template client

---
+++ react/client/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.NODE_PORT || 8080);
